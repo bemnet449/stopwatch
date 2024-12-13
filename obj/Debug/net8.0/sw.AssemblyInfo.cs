0fa38754d7ca8649c8de379e395644ee70f5652a
bemnet <BemnetSin510@gmail.com>
2024-12-13 21:00:25 +0300

    final

--- obj/Debug/net8.0/sw.AssemblyInfo.cs
+++ obj/Debug/net8.0/sw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4f3d18b1c5b51788bc8441d28a9640a27d2b37e")]
 [assembly: System.Reflection.AssemblyProductAttribute("sw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
